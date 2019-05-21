ebc45fb0df0db68679b31548ac9f750913874fdf
AndreyPerekosov <perekosov@gmail.com>
2019-05-21 20:48:22 +0300

    Change SSL

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -142,7 +142,7 @@ if os.environ.get('APP_LOCATION') == 'heroku':
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
     EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
-    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = True
 else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'your_account@gmail.com'
